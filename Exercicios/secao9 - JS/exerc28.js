ff157161fa16d0bf5716b2c127e24d183703b380
Merieli Manzano <merieli.dev@gmail.com>
2022-03-22 17:59:08 -0300

    exercicio 28 segunda opcao de resolucao

--- Exercicios/secao9 - JS/exerc28.js
+++ Exercicios/secao9 - JS/exerc28.js
@@ -6,3 +6,18 @@ const segundoMaior = (listaNumeros) => {
 };
 console.log(segundoMaior([12, 16, 1, 5]));
 console.log(segundoMaior([8, 4, 5, 6]));
+
+//------- OU:
+function segundoMaior(numeros) {
+    let indiceDoMaior = 0;
+    let segundoMaior;
+    numeros.forEach((numero, indice) => {
+        if (numero > numeros[indiceDoMaior]) indiceDoMaior = indice;
+    });
+    numeros.splice(indiceDoMaior, 1);
+    segundoMaior = numeros[0];
+    numeros.forEach((numero) => {
+        if (numero > segundoMaior) segundoMaior = numero;
+    });
+    return segundoMaior;
+}
