109f7fb3694122181dc9f0911c682bb18fc52912
Merieli Manzano <merieli.dev@gmail.com>
2022-04-08 13:26:34 -0300

    Aula 480 a 487

---
+++ VueJS/calculator/src/components/Button.vue
@@ -0,0 +1,54 @@
+<template>
+  <button class="button"
+    :class="{ doble, triple, operation }"> <!--Nesse caso se doble ou triple ou operation for "true" será aplicada uma classe com mesmo nome do atributo "como a classe terá o mesmo nome que a propriedade condicional nessse caso é possível escrever só a propriedade" -->
+      {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {}, //string vazia que será passada como propriedade do componente
+    operation: { type: Boolean },
+    double: { type: Boolean },
+    triple: { type: Boolean },
+  }
+}
+</script>
+
+<style>
+:root{
+  --bg-button: #f0f0f0;
+  --border-button: solid 1px #888;
+}
+
+.button{
+  background-color: var(--bg-button);
+  border: none;
+  border-right: var(--border-button);
+  border-bottom: var(--border-button);
+  font-size: 1.4rem;
+  outline: none;
+}
+
+.button:active{
+  background-color: #ccc;
+}
+
+.button.double{
+  grid-column: span 2;
+}
+
+.button.triple{
+  grid-column: span 3;
+}
+
+.button.operation{
+  background-color: #fa8231;
+  color: #fff;
+}
+
+.button.operation:active{
+  background-color: #fa8231cc;
+}
+</style>
---
+++ VueJS/calculator/src/main/Calculator.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="calculator">
+        <Display value="1000" />
+        <Button label="AC" triple />
+        <Button label="/" operation />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" operation />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" operation />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" operation />
+        <Button label="0" double />
+        <Button label="." />
+        <Button label="=" operation />
+    </div>
+</template>
+
+<script>
+import Display from "../components/Display"
+import Button from "../components/Button"
+
+//para usar os componentes importados é preciso registrá-los dentro do export default
+export default { 
+    components: { Button, Display }
+}
+</script>
+
+<style>
+.calculator{
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr repeat(5, 48px);
+    border-radius: 5px;
+    display: grid;
+    overflow: hidden;
+    height: 320px;
+    width: 235px;
+}
+</style>
